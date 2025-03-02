f3cb675081acd183b4dddc40e30d8c4b75ab2c98
aemanskova <ae.manskova@mail.ru>
2025-03-02 13:36:08 +0300

    header fixes

--- frontend/src/widgets/ui/Footer/Footer.tsx
+++ frontend/src/widgets/ui/Footer/Footer.tsx
@@ -16,52 +16,52 @@ const Footer: React.FC = () => {
                         className={styles.footer__logo}
                     />
                 </Link>
-                <ul className={styles.footer__links}>
-                    <li>
-                        <NavLink className={styles.footer__link} to={'/'}>
-                            О нас
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={styles.footer__link} to={'/'}>
-                            FAQ
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={styles.footer__link} to={'/'}>
-                            Пользовательское соглашение
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className={styles.footer__link} to={'/'}>
-                            Контакты
-                        </NavLink>
-                    </li>
+                {/*<ul className={styles.footer__links}>*/}
+                {/*    <li>*/}
+                {/*        <NavLink className={styles.footer__link} to={'/'}>*/}
+                {/*            О нас*/}
+                {/*        </NavLink>*/}
+                {/*    </li>*/}
+                {/*    <li>*/}
+                {/*        <NavLink className={styles.footer__link} to={'/'}>*/}
+                {/*            FAQ*/}
+                {/*        </NavLink>*/}
+                {/*    </li>*/}
+                {/*    <li>*/}
+                {/*        <NavLink className={styles.footer__link} to={'/'}>*/}
+                {/*            Пользовательское соглашение*/}
+                {/*        </NavLink>*/}
+                {/*    </li>*/}
+                {/*    <li>*/}
+                {/*        <NavLink className={styles.footer__link} to={'/'}>*/}
+                {/*            Контакты*/}
+                {/*        </NavLink>*/}
+                {/*    </li>*/}
 
-                    <li>
-                        <NavLink className={styles.footer__link} to={'/'}>
-                            Разработчикам
-                        </NavLink>
-                    </li>
+                {/*    <li>*/}
+                {/*        <NavLink className={styles.footer__link} to={'/'}>*/}
+                {/*            Разработчикам*/}
+                {/*        </NavLink>*/}
+                {/*    </li>*/}
 
-                    <li>
-                        <NavLink className={styles.footer__link} to={'/'}>
-                            Политика конфиденциальности
-                        </NavLink>
-                    </li>
-                </ul>
-                <ul className={styles.footer__socials}>
-                    <li>
-                        <Link className={styles.footer__link} to={'/'}>
-                            <img src={vkLogo} alt="Иконка VK" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={styles.footer__link} to={'/'}>
-                            <img src={tgLogo} alt="Иконка TG" />
-                        </Link>
-                    </li>
-                </ul>
+                {/*    <li>*/}
+                {/*        <NavLink className={styles.footer__link} to={'/'}>*/}
+                {/*            Политика конфиденциальности*/}
+                {/*        </NavLink>*/}
+                {/*    </li>*/}
+                {/*</ul>*/}
+                {/*<ul className={styles.footer__socials}>*/}
+                {/*    <li>*/}
+                {/*        <Link className={styles.footer__link} to={'/'}>*/}
+                {/*            <img src={vkLogo} alt="Иконка VK" />*/}
+                {/*        </Link>*/}
+                {/*    </li>*/}
+                {/*    <li>*/}
+                {/*        <Link className={styles.footer__link} to={'/'}>*/}
+                {/*            <img src={tgLogo} alt="Иконка TG" />*/}
+                {/*        </Link>*/}
+                {/*    </li>*/}
+                {/*</ul>*/}
             </nav>
         </footer>
     )
