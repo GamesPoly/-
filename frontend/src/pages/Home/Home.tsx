46e65795e38671c9c3904bbc9e36ce66bad7a9d8
danilmash <danil.mashentsev@mail.ru>
2025-03-10 18:47:59 +0300

    Секция Програмное обеспечение игровой компьютерной индустрии в Московском Политехе  на главной странице

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -4,10 +4,12 @@ import Hero from './Hero/Hero'
 import Collection from './Collection/Collection'
 import Genre from './Genre/Genre'
 import Functions from './Functions/Functions'
+import ProgramExample from './ProgramExample/ProgramExample'
 const Home = () => {
     return (
         <main className={styles['home__wrapper']}>
             <Hero />
+            <ProgramExample />
             <Collection />
             <Genre />
             <Functions />
