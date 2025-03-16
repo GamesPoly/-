9017ef2e56e8b8afa41ac90a6e8496f40d28dd51
duh-git <cherebeevkarim2015@gmail.com>
2025-03-16 12:42:56 +0300

    done courses section

--- frontend/src/pages/Home/Courses/Courses.tsx
+++ frontend/src/pages/Home/Courses/Courses.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import styles from './Courses.module.scss'
-import course_bg from '../../../shared/assets/course-bg.png'
+import { CoursesCard } from './CoursesCard/CoursesCard'
 import { ArrowLink } from '../../../shared/ui/ArrowLink/ArrowLink'
+import course_bg from './images/course-bg.png'
+import i1 from './images/i1.png'
+import i2 from './images/i2.png'
+import i3 from './images/i3.png'
+import i4 from './images/i4.png'
+
+const cards = [
+    {
+        header: 'Детский технопарк',
+        description:
+            'Инженерно-технологический комплекс, на базе которого проводятся углубленные технико-ориентированные курсы дополнительного образования для школьников.',
+        imgSrc: i1,
+    },
+    {
+        header: 'Лаборатория «Летающая Робототехника»',
+        description:
+            'Курсы по конструированию, сборке, настройке, пилотированию и программированию квадрокоптеров, соревнования и подготовка к ЕГЭ по информатике и ИКТ.',
+        imgSrc: i2,
+    },
+    {
+        header: 'Junior Campus',
+        description:
+            'Программа Junior Campus, разработанная в партнерстве с Московским политехническим университетом, позволит детям от 6 до 14 лет в интерактивном режиме узнать о безопасном и ответственном поведении на дороге, об устройстве автомобилей и экологичном транспорте.',
+        imgSrc: i3,
+    },
+    {
+        header: 'Центр довузовского образования и подготовки к ЕГЭ',
+        description:
+            'Подготовка к сдаче ЕГЭ, ОГЭ и внутренних вступительных испытаний по информатике, математике, физике, русскому языку и обществознанию. Программы для школьников, студентов колледжей, граждан иностранных государств.',
+        imgSrc: i4,
+    },
+]
 
 const Courses = () => {
     return (
-        <section className={styles['courses']}>
-            <div className={styles['courses__header']}>
+        <section className={styles.courses}>
+            <div className={styles.courses__header}>
                 <h2 className={styles['courses__header_up']}>
                     Подготовительные курсы
                 </h2>
@@ -16,7 +48,7 @@ const Courses = () => {
             </div>
             <div className={styles['courses__descryption-container']}>
                 <div
-                    className={styles['courses__descryption']}
+                    className={styles.courses__descryption}
                     style={{ backgroundImage: `url(${course_bg})` }}
                 >
                     <div className={styles['courses__descryption-card']}>
@@ -32,10 +64,14 @@ const Courses = () => {
                             общественностью и другие. Обучение рисунку,
                             живописи, композиции и иллюстрации с нуля.
                         </p>
+                        <ArrowLink to="/" className={styles.link} />
                     </div>
                 </div>
-                <ul className={styles['courses__list']}>
-                    <li></li>
+                <ul className={styles.courses__list}>
+                    {cards.map((card) => (
+                        <CoursesCard key={card.header} {...card} />
+                    ))}
+                    <div className={styles.gradient}></div>
                 </ul>
             </div>
         </section>
